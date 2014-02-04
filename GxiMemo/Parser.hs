ff4068b25a3a8e6204d6bff164e04746ab6a9b95
Shou Ya <shouyatf@gmail.com>
2014-02-05 05:23:31 +0800

    gratulon! gxi povas analizi mem nun!

--- GxiMemo/Parser.hs
+++ GxiMemo/Parser.hs
@@ -2,46 +2,48 @@
 import GxiMemo.SyntaxNode
 
 rules =
-  [("main", Sequence [Rule "spaces",
-                      anyTimes (Sequence [Rule "rule",
-                                          manyTimes $ Rule "newline"]),
-                      Rule "rule",
-                      Rule "spaces"])
-  ,("space", Choice (map Atom [" ", "\t", "\r", "\n"]))
+  [("main", Sequence [Rule "spaces", Rule "rule",
+                      anyTimes (Sequence [manyTimes (Rule "newline"),
+                                          Rule "rule"])])
+  ,("space", Choice (map Atom [" ", "\t", "\r\n", "\r", "\n"]))
+  ,("space_nonl", Choice (map Atom [" ", "\t"]))
   ,("spaces", anyTimes (Rule "space"))
+  ,("spaces_nonl", anyTimes (Rule "space_nonl"))
   ,("newline", Choice (map Atom ["\r\n", "\r", "\n"]))
   ,("alpha", Choice (map (Atom . (:[])) (['a'..'z'] ++ ['A'..'Z'])))
   ,("alpha_underscore", Choice [Rule "alpha", Atom "_"])
   ,("digit", Choice (map (Atom . (:[])) ['0'..'9']))
   ,("integer", manyTimes (Rule "digit"))
-  ,("token", Sequence [Rule "alpha",
-                       anyTimes (Choice [Rule "alpha", Rule "digit"])])
-  ,("rule", Sequence [Rule "spaces", Rule "token",
-                      Rule "spaces", Atom "=",
-                      Rule "spaces", Rule "sequence", Rule "spaces"])
-  ,("seequence", Sequence [anyTimes (Sequence [Rule "expression", Rule "spaces"]),
-                           Rule "expression"])
-  ,("expression", Choice [Sequence [Atom "(", Rule "spaces", Rule "sequence",
-                                    Rule "spaces", Atom ")"],
-                          Rule "repetition", -- left-recursion
-                          Rule "choice",
-                          Rule "pos_lookahead",
-                          Rule "neg_lookahead",
-                          Rule "string",
-                          Rule "rule"])
-  ,("string", Sequence [Atom "\"", anyTimes (Rule "char"), Atom "\""])
+  ,("token", Sequence [Rule "alpha_underscore",
+                       anyTimes (Choice [Rule "alpha_underscore", Rule "digit"])])
   ,("char", Choice ([Rule "alpha", Rule "digit"] ++
-                    (map (Atom . (:[])) "~`!@#$%^&*()-_=+[]{}:\";'<>?,./") ++
+                    (map (Atom . (:[])) " ~`!@#$%^&*()-_=+[]{}:;'<>?,./|") ++
                     [Atom "\\\\", Atom "\\\"",
                      Sequence [Atom "\\", Rule "alpha"]]))
-  ,("repetition", Choice [Sequence [Rule "expression", Atom "?"],
-                          Sequence [Rule "expression", Atom "+"],
-                          Sequence [Rule "expression", Atom "*"],
-                          Sequence [Rule "expression", Atom "{",
-                                    optional (Rule "number"), Atom ",",
-                                    optional (Rule "number"), Atom "}"]])
-  ,("choice", Sequence [Rule "expression", Rule "spaces", Atom "|",
-                        Rule "spaces", Rule "expression"])
+
+
+  ,("rule", Sequence [Rule "spaces", Rule "token",
+                      Rule "spaces", Atom "=",
+                      Rule "spaces", Rule "sequence", Rule "spaces_nonl"])
+  ,("sequence", Sequence [Rule "expression",
+                          anyTimes (Sequence [Rule "spaces_nonl",
+                                              Rule "expression"])])
+  ,("expression", Sequence [Rule "expression1", anyTimes (Rule "choice_mark")])
+  ,("expression1", Sequence [Rule "expression2",
+                             optional (Rule "repetition_mark")])
+  ,("expression2", Choice [Sequence [Atom "(", Rule "spaces", Rule "sequence",
+                                     Rule "spaces", Atom ")"],
+                           Rule "string",
+                           Rule "token",
+                           Rule "pos_lookahead",
+                           Rule "neg_lookahead"])
+  ,("repetition_mark", Choice [Atom "?", Atom "+", Atom "*",
+                               Sequence [Atom "{", optional (Rule "number"),
+                                         Atom ",", optional (Rule "number"),
+                                         Atom "}"]])
+  ,("choice_mark", Sequence [Rule "spaces", Atom "|",
+                             Rule "spaces", Rule "expression"])
+  ,("string", Sequence [Atom "\"", anyTimes (Rule "char"), Atom "\""])
   ,("pos_lookahead", Sequence [Atom "@", Rule "expression"])
   ,("pos_lookahead", Sequence [Atom "!", Rule "expression"])
   ]
